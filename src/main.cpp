8b59d92eddf7f16ad07b4fe197d84f291c553098
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-10-03 06:38:11 +0530

    support 1.6.629+

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,8 @@ extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
 	v.PluginVersion(Version::MAJOR);
 	v.PluginName("Enhanced Death Cam");
 	v.AuthorName("powerofthree");
-	v.UsesAddressLibrary(true);
+	v.UsesAddressLibrary();
+	v.UsesUpdatedStructs();
 	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
 
 	return v;
